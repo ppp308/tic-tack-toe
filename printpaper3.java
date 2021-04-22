6d9a6917baeddc56f55e5ea7e78435fff58dbf43
ppp308 <81544644+ppp308@users.noreply.github.com>
2021-04-23 08:52:35 +0900

    영수증

---
+++ printpaper3.java
@@ -0,0 +1,173 @@
+package basic3;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+//5강.리포팅 연습 2021.04.22 박재희
+//영수증 출력3
+public class printpaper1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat sc = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		// 현재 시간 출력
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:dd");
+		// 시간도 포함하여 출력
+		SimpleDateFormat sd = new SimpleDateFormat("yyyyMMdd");
+		
+		DecimalFormat sb = new DecimalFormat("###,###,###");
+		// 천에 자리마다 콤마를 찍음
+		Calendar cal = Calendar.getInstance();
+		// 캘린더 자체를 옴
+		Date time = new Date();
+		// 현재 날짜를 가져옴
+		cal.add(Calendar.HOUR, -1);
+		// 캘린더를 가져와서 시간에서 -1을 함
+		String k11_date = sc.format(time);
+		// 오늘날짜만 있는 형태를 가져옴
+		String k11_Cartime = sf.format(cal.getTime());
+		// -1 한 시간도 포함하여 출력
+		String k11_today = sd.format(time);
+		String[] k11_itemname = { "해피홈SAFE365핸드폰클리너", "(G)LH원형유리화병10", "상투과자E", "더블링나일론깡롤", "해피홈SAFE핸드워시",
+				"농심신라면120g*5", "(대)국내산삼겹살찌개", "오뚜기진비빔면156g", "GAP 죽장사과 4-6입/", "순유기농 바나나(봉)", "피코크 탄두리 닭가슴",
+				"(달콤)순살닭강정(g)", "DZ주니어양말3족_BK", "DZ주니어양말3족_WK", "프리미엄생연어초밥", "피코크들깨미역국500", "남양맛있는우유GT저지방",
+				"녹차원흑당시럽10입", "진라면소컵(매)65g*6", "케라시스앰플트리트", "푸르밀미숫가루우유", "자일로스갈색설탕500g", "CJ 비비고칩 20G", "피코크들깨미역국500g",
+				"흑원당말차밀크티", "1+등급란10개입 특", "동원양반볶음김치기획", "바나나", "초코파이", "포도","맛없는 자이로스 ()100그램a++" };
+		// 품목 변수 선언
+		int[] k11_price = { 7500, 6900, 5980, 3300, 1000000, 3380000, 6610, 2750, 10,100, 2480, 7920, 4980, 4980, 7130,
+				3980, 4480, 4980, 3210, 6950, 2980, 1990, 1980, 3980, 4480, 3480, 7980, 1000, 2500, 3300,4000 };
+		// 가격 변수 선언
+		int[] k11_num = { 2, 1, 1, 1, 5, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 4, 1,1 };
+		// 수량 변수 선언
+		boolean[] k11_taxfree = { false, false, false, false, false, false, true, false, true, true, false, false,
+				false, false, false, false, true, false, false, false, false, false, false, false, false, true, false,
+				false, false, false,false };
+		// 면세 등을 결정하는 변수
+		int k11_itemcount = 0;
+		// 수량 변수 초기화
+		int k11_taxfreetotal = 0;
+		// 면세 총합 초기화
+		int k11_taxtotal = 0;
+		// 세금 초기화
+		double k11_sum = 0;
+		// 합계 초기화
+		double k11_beforetaxtotal =0;
+		//과세물품에서 부가세를 뺀 금액 초기화
+		double k11_beforetax =0;
+		//과세물품에의 부가세 초기화
+		System.out.printf("emart");
+		System.out.printf("이마트 죽전점 (031)888-1234\n");
+		System.out.printf("     206-86-50913 강희석\n");
+		System.out.printf("     용인 수지구 포은대로 552\n");
+		System.out.printf("영수증 미지참시 교환/환불 불가\n");
+		System.out.printf("정상상품에 한함, 30일 이내(신선 7일)\n");
+		System.out.printf("※일부 브랜드매장 제외(매장 고지물참조)\n");
+		System.out.printf("교환/환불 구매점에서 가능(결제카드 지참)\n");
+		System.out.printf("\n");
+		System.out.printf("[구 매]%s      POS:0011-9861\n", k11_date);
+		// 현재 시스템상 날짜,시간을 가져옴
+		System.out.printf("-----------------------------------------\n");
+		System.out.printf("  상품명          단가    수량     금액\n");
+		System.out.printf("-----------------------------------------\n");
+		
+		
+		for (int i = 0; i < k11_itemname.length; i++) {
+			// 품목명으로 선언한 변수 길이만큼 출력
+			// 0~29
+			if (k11_itemname[i].length() >= 7) {
+				// 각 품목명의 길이가 7보다 클때
+				k11_itemname[i] = k11_itemname[i].substring(0, 6);
+				// 각 품목명의 6까지 가져옴
+			} else {
+
+			}
+
+			int num = 12 - k11_itemname[i].getBytes().length;
+			// 만약에 품목명이 모두 한글로 차있다면 6*2=12
+			// 12 - 품목명길이의 바이트
+			// 6길이 글자 - 실제 글자 길이 = 공백
+			for (int j = 0; j < num; j++) {
+				k11_itemname[i] = k11_itemname[i] + " ";
+				// 공백의 길이만큼 글자+공백을 만듬
+			}
+
+			if (k11_taxfree[i] == true) {
+				// 만약에 면세 품목이라면
+				System.out.printf("*%s %9s %5d %10s\n", k11_itemname[i], sb.format(k11_price[i]), k11_num[i],
+						sb.format(k11_price[i]*k11_num[i]));
+				// 별표를 붙이고 출력함
+
+			} else {
+				// 면세 품목이 아니라면
+				System.out.printf(" %s %9s %5d %10s\n", k11_itemname[i], sb.format(k11_price[i]), k11_num[i],
+						sb.format(k11_price[i]*k11_num[i]));
+				// 별표를 붙이지 않고 출력
+			}
+
+			if ((i + 1) % 5 == 0) {
+				// 만약에 품목의 개수가 5라면
+				System.out.printf("-----------------------------------------\n");
+				// 5개 마다 해당 출력문이 나오게 함
+			} else {
+				// 그게 아니라면 그냥 출력
+			}
+
+		}
+
+		for (int i = 0; i < k11_num.length; i++) {
+			// 총 품목수량의 길이만큼 돌아감
+			k11_itemcount++;
+			// count를 모두 더하여 출력
+		}
+		System.out.printf("                총 품목 수량%12d\n", k11_itemcount);
+		// 결과 출력
+		
+		for (int i = 0; i < k11_itemname.length; i++) {
+			// 품목명의 길이만큼 돌면서
+			if (k11_taxfree[i] == true) {
+				// 만약에 면세 품목이라면
+				k11_taxfreetotal = k11_taxfreetotal + k11_price[i] * k11_num[i];
+				// 해당과 같은 계산을 하고 taxfreetotal에 더함
+			} else {
+				k11_taxtotal = k11_taxtotal + k11_price[i] * k11_num[i];
+				// 그게 아니라면 taxtotal에 저장하며
+				k11_beforetaxtotal = k11_taxtotal / (11.0/10.0);
+				k11_beforetax = k11_taxtotal-k11_beforetaxtotal;
+				// 과세가격의 세금을 따로 구함			
+			}
+			k11_sum = k11_beforetaxtotal + k11_taxfreetotal+k11_beforetax;
+			// 총 결제대상금액은 면세품목 + 과세품목(세전가격)+부가세
+		}
+	
+		System.out.printf("             (*)면 세  물 품%12s\n", sb.format(k11_taxfreetotal));
+		// 천자리마다 콤마를 찍음
+		System.out.printf("                과 세  물 품%12s\n", sb.format(k11_beforetaxtotal));
+		// 천자리마다 콤마를 찍음
+		System.out.printf("                부   가   세%12s\n", sb.format(k11_beforetax));
+		// 천자리마다 콤마를 찍음
+		System.out.printf("                합        계%12s\n", sb.format(k11_sum));
+		// 천자리마다 콤마를 찍음
+		System.out.printf("결제대상금액%28s\n", sb.format(k11_sum));
+		// 천자리마다 콤마를 찍음
+		System.out.printf("-----------------------------------------\n");
+		System.out.printf("0012KEB하나        541707**0484/35860658\n");
+		System.out.printf("카드결제(IC)            일시불/%s\n", sb.format(k11_sum));
+		// 천자리마다 콤마를 찍음
+		System.out.printf("-----------------------------------------\n");
+		System.out.printf("              [신세계포인트 적립]\n");
+		System.out.printf("박*희 고객님의 포인트 현황입니다.\n");
+		System.out.printf("금회발생포인트         9350**9995    164\n");
+		System.out.printf("누계(가용)포인트          5,637(   5,473)\n");
+		System.out.printf("*신세계포인트 유효기간은 2년입니다.\n");
+		System.out.printf("-----------------------------------------\n");
+		System.out.printf("    구매금액기준 무료주차시간 자동부여\n");
+		System.out.printf("차량번호:                       34저****\n");
+		System.out.printf("입차시간:            %s\n", k11_Cartime);
+		System.out.printf("-----------------------------------------\n");
+		System.out.printf("캐셔:084599 양oo                   1150\n");
+		System.out.printf("      lIllIllllIllllIllIllllIllIllllIllI \n");
+		System.out.printf("       %s/00119861/00164980/31\n",k11_today);
+	}
+}
---
+++ printrpaper2.java
@@ -0,0 +1,185 @@
+package basic3;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+//5강.리포팅 연습 2021.04.22 박재희
+//영수증 출력2
+public class printrpaper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat sc = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		//원하는 형태로 출력하기 위해 불러옴
+		SimpleDateFormat sf = new SimpleDateFormat("MM월dd일");
+		//14일 이후의 날짜를 원하는 형태로 출력하기 위해 불러옴
+		Calendar cal = Calendar.getInstance();
+		//날짜 함수 불러오기
+		DecimalFormat sb = new DecimalFormat("###,###,###");
+		//1000자리마다 콤마찍기
+		Date time = new Date();
+		//오늘날짜 가져오기
+		cal.setTime(time);
+		//오늘날짜 불러오기
+		String k11_date = sc.format(cal.getTime());
+		//오늘의 날자를 원하는 형태로 출력해서 변수에 저장
+		cal.add(Calendar.DATE, 14);
+		//일수에 14일을 더하기 
+		String k11_afterdate = sf.format(cal.getTime());
+		//14일 더한 날짜를 원하는 형태로 가져와서 변수에 저장
+		String k11_itemname1 = "퓨어에어 비말차단용마스크(최고급형)";
+		//첫번째 품목 저장
+		String k11_itemcode1 = "1031615";
+		//첫번째 품목코드 저장
+		int k11_price1 = 3000;
+		//첫번째 가격 저장
+		int k11_amount1 = 1;
+		//첫번째 품목의 수량 저장
+		String k11_itemname2 = "슬라이드식명찰(가로형)(100호)";
+		//두번째 품목 저장
+		String k11_itemcode2 = "11008152";
+		//두번째 품목의 코드 저장
+		int k11_price2 = 1000;
+		//두번째 품목 가격
+		int k11_amount2 = 1;
+		//두번째 품목 수량
+		String k11_itemname3 = "매직흡착 인테리어후크(알루미늄타입)";
+		//세번째 품목 
+		String k11_itemcode3 = "1022760";
+		//세번째 품목 코드
+		int k11_price3 = 1000;
+		//세번째 품목 코드 가격
+		int k11_amount3 = 1;
+		//세번재 품목 수량 
+		double k11_total = k11_price1 * k11_amount1 + k11_price2 * k11_amount2 + k11_price3 * k11_amount3;
+		//가격*수량 = 총 가격 
+		double k11_totaltaxprice = (k11_total / 1.1);
+		//총가격에서 1.1을 곱하여 세금을 제외한 가격 구하기
+		double k11_totaltax = (k11_total - (k11_total / 1.1));
+		//총 가격 - 세금 제외한 가격 = 세금
+		System.out.printf("          \"국민가게, 다이소\"\n");
+		System.out.printf("(주)아성다이소_분당서현점\n");
+		System.out.printf("전화:031-702-6016\n");
+		System.out.printf("본사:서울 강남구 남부순환로 2748 (도곡동)\n");
+		System.out.printf("대표:박정부,신호섭 213-81-52063\n");
+		System.out.printf("매장:경기도 성남시 분당구 분당로53번길 11(서현동\n");
+		System.out.printf("=========================================\n");
+		System.out.printf("       소비자중심경영(CCM)인증기업\n");
+		System.out.printf("     ISO 9001 품질경영시스템 인증기업\n");
+		System.out.printf("=========================================\n");
+		System.out.printf("        교환/환불 14일(%s)이내,\n", k11_afterdate);
+		//14일 이후에 날짜가 자동으로 바뀌고 월,일로 출력
+		System.out.printf("(전자)영수증,결제카드 지참 후 구입매장에서 가능\n");
+		System.out.printf("   포장/가격 택 훼손시 교환/환불 불가\n");
+		System.out.printf("      체크카드 취소 시 최대 7일 소요\n");
+		System.out.printf("=========================================\n");
+		System.out.printf("[POS 1058231]        %s\n", k11_date);
+		//시스템 날짜 출력
+		System.out.printf("=========================================\n");
+		
+		for (int i = 0; i < k11_itemname1.length(); i++) {
+			// 품목명으로 선언한 변수 길이만큼 출력
+			// 0~29
+			if (i >= 7) {
+				// 각 품목명의 길이가 7보다 클때
+				k11_itemname1 = k11_itemname1.substring(0, 5);
+				// 각 품목명의 5까지 가져옴
+			} else {
+
+			}
+
+			int num = 10 -  k11_itemname1.getBytes().length;
+			// 만약에 품목명이 모두 한글로 차있다면 5*2=10
+			// 10 - 품목명길이의 바이트
+			// 6길이 글자 - 실제 글자 길이 = 공백
+			for (int j = 0; j < num; j++) {
+				 k11_itemname1 =  k11_itemname1 + " ";
+				// 공백의 길이만큼 글자+공백을 만듬
+			}
+		}
+		System.out.printf("%s  %9s %8s %10s\n", k11_itemname1, sb.format(k11_price1), sb.format(k11_amount1),
+				sb.format(k11_price1));
+		//품목의 길이가 15까지 나올 수 있게 출력하고, 숫자의 띄어쓰기를 맞춤
+		System.out.printf("[%s]\n", k11_itemcode1);
+		//코드 출력
+		for (int i = 0; i < k11_itemname2.length(); i++) {
+			// 품목명으로 선언한 변수 길이만큼 출력
+			// 0~29
+			if (i >= 7) {
+				// 각 품목명의 길이가 7보다 클때
+				k11_itemname2 = k11_itemname2.substring(0, 5);
+				// 각 품목명의 5까지 가져옴
+			} else {
+
+			}
+
+			int num = 10 -  k11_itemname2.getBytes().length;
+			// 만약에 품목명이 모두 한글로 차있다면 5*2=10
+						// 10 - 품목명길이의 바이트
+						// 6길이 글자 - 실제 글자 길이 = 공백
+			for (int j = 0; j < num; j++) {
+				 k11_itemname2 =  k11_itemname2 + " ";
+				// 공백의 길이만큼 글자+공백을 만듬
+			}
+		}
+				
+		System.out.printf("%s  %9s %8s %10s\n", k11_itemname2, sb.format(k11_price2), sb.format(k11_amount2),
+				sb.format(k11_price2));
+		//품목의 길이가 15까지 나올 수 있게 출력하고, 숫자의 띄어쓰기를 맞춤
+		System.out.printf("[%s]\n", k11_itemcode2);
+		//코드 출력
+		for (int i = 0; i < k11_itemname3.length(); i++) {
+			// 품목명으로 선언한 변수 길이만큼 출력
+			// 0~29
+			if (i >= 7) {
+				// 각 품목명의 길이가 7보다 클때
+				k11_itemname3 = k11_itemname3.substring(0, 5);
+				// 각 품목명의 5까지 가져옴
+			} else {
+
+			}
+
+			int num = 10 -  k11_itemname3.getBytes().length;
+			// 만약에 품목명이 모두 한글로 차있다면 5*2=10
+						// 10 - 품목명길이의 바이트
+						// 6길이 글자 - 실제 글자 길이 = 공백
+			for (int j = 0; j < num; j++) {
+				 k11_itemname3 =  k11_itemname3 + " ";
+				// 공백의 길이만큼 글자+공백을 만듬
+			}
+		}
+		
+		System.out.printf("%s  %9s %8s %10s\n", k11_itemname3, sb.format(k11_price3), sb.format(k11_amount3),
+				sb.format(k11_price3));
+		//품목의 길이가 15까지 나올 수 있게 출력하고, 숫자의 띄어쓰기를 맞춤
+		System.out.printf("[%s]\n", k11_itemcode3);
+		//코드 출력
+		System.out.printf("             과세합계%20s\n", sb.format(k11_totaltaxprice));
+		//과세합계에 콤마를 반영하여 출력
+		System.out.printf("               부가세%20s\n", sb.format(k11_totaltax));
+		//부가세에 콤마를 반영하여 출력
+		System.out.printf("-----------------------------------------\n");
+		System.out.printf("판매합계   %30s\n", sb.format(k11_total));
+		//판매합계에 콤마를 반영하여 출력
+		System.out.printf("=========================================\n");
+		System.out.printf("신용카드   %30s\n", sb.format(k11_total));
+		//신용카드에 판매합계를 콤마를 반영하여 출력
+		System.out.printf("-----------------------------------------\n");
+		System.out.printf("우리카드                 538720**********\n");
+		System.out.printf("승인번호 77982843(0)    승인금액 %8s\n", sb.format(k11_total));
+		//가격에 콤마를 반영하여 출력
+		System.out.printf("=========================================\n");
+		System.out.printf("         %s 분당서현점\n", k11_date);
+		//시스템 날짜 출력
+		System.out.printf("상품 및 기타 문의: 1522-4400\n");
+		System.out.printf("멤버십 및 샵다이소 관련 문의 : 1599-2211\n");
+		System.out.printf("        llIllllIllllIllllIlllll\n");
+		//바코드는 제대로 출력하지 못함..
+		System.out.printf("         2112820610158231\n");
+		System.out.printf("----------------------------------------\n");
+		System.out.printf("◈다이소 멤버십 앱 또는 홈페이지에 접속하셔서 회원가입후 다양한 혜택을 누려보세요!◈\n");
+
+	}
+
+}
